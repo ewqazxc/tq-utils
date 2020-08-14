f4ed77d1e8d0ca07a224e2cfc156e06512fe2326
XC <603550500@qq.com>
2020-08-14 21:10:03 +0800

    merge

--- src/ImageUtils/index.ts
+++ src/ImageUtils/index.ts
@@ -5,11 +5,12 @@ export interface ImageUtils {
   compressorImageBySize: (file: File | Blob, size?: number) => File;
 }
 
-export const compressorImage = async (file: File | Blob, quality = 0.6) => {
+export const compressorImage = async (file: File | Blob, quality = 0.6, options?: ImageCompressor.Options) => {
   const newFile = await new Promise<File | Blob>(async (resolve, reject) => {
     new ImageCompressor(
       file,
       {
+        ...options,
         quality: quality,
         async success(result: File) {
           var form = new FormData();
@@ -25,25 +26,15 @@ export const compressorImage = async (file: File | Blob, quality = 0.6) => {
   return newFile;
 }
 
-export const compressorImageBySize = async (file: File | Blob, size = 1024 * 1024) => {
+export const compressorImageBySize = async (file: File | Blob, size = 1024 * 1024, maxWidth = 960) => {
   let quality = .6;
-  let newFile: any = await new Promise<File | Blob>(async (resolve, reject) => {
-    new ImageCompressor(
-      file,
-      {
-        quality: quality,
-        convertSize: size,
-        async success(result: File) {
-          var form = new FormData();
-          form.append('file', result, result.name);
-          resolve(result);
-        },
-        error(e) {
-          console.log(e.message);
-          reject();
-        },
-      });
-  });
+  let newFile = await compressorImage(file, quality, { maxWidth });
+  while (newFile.size > size && quality >= 0) {
+    quality = Number((quality - 0.1).toFixed(1));
+    if (quality >= 0) {
+      newFile = await compressorImage(newFile, quality, { maxWidth });
+    }
+  }
   while (newFile.size > size) {
     quality = Number((quality - 0.1).toFixed(1));
     if (quality >= 0) {
